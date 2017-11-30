51d29f55e6f6486b953ef5da0b2a4cc745fd4b46
Robert Rossol <shyguyrbr1@gmail.com>
2017-11-30 14:05:54 -0600

    token based auth with JWT complete

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -14,6 +14,7 @@ module ExceptionHandler
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
     rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
@@ -32,5 +33,9 @@ module ExceptionHandler
   # JSON response with message; status code 401 - Unauthorized
   def unauthorized_request(e)
     json_response({ message: e.message }, :unauthorized)
+  end
+  # JSON response with message; Status code 498 - Invalid Token
+  def four_ninety_eight(e)
+    json_response({ message: e.message }, :invalid_token)
   end
 end
